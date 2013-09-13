7decefedbbde1ff5f134e3463590264d691f1dcc
AJ Slater <aj@slater.net>
2013-09-13 14:47:09 -0700

    rest of term sign spacing

--- src/com/swtanalytics/math/Term.java
+++ src/com/swtanalytics/math/Term.java
@@ -9,6 +9,24 @@ public class Term {
 		this.coefficient = c;
 		this.exponent = e;
 	}
+
+    public String prettyPrint(boolean isFirstTerm) {
+            // Not smart about string construction. Just dumb manipulation
+
+            String result = this.toString();
+            if (!isFirstTerm) {
+                result = result.substring(0,1) + ' '
+                         + result.substring(1, result.length());
+            }
+/* Optional first term 
+            else {
+                if (this.c >= 0) {
+                    result = result.substring(1, result.lenght());
+            }
+*/
+
+            return result;
+    }
 	
 	public String toString(){
 		return String.format("%+dx^%d", this.coefficient, this.exponent);
