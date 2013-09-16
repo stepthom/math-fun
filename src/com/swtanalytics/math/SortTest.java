668cc1e15b28e47bc35a030c705645d9134df89f
AJ Slater <aj@slater.net>
2013-09-16 11:55:30 -0700

    add skeleton for Sorting Test

---
+++ src/com/swtanalytics/math/SortTest.java
@@ -0,0 +1,21 @@
+package com.swtanalytics.math;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SortTest {
+
+    Term[] TERMS = {
+        new Term(1,1),
+        new Term(1,2),
+        new Term(1,3),
+        new Term(1,4),
+        new Term(1,5),
+    };
+
+    @Test
+    public void pass() {
+    }
+}
