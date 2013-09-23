a7fbef48be475b1f531345180453bc34d14bc98a
ctcaldwell <tcaldwell@Todds-MacBook-Pro.local>
2013-09-23 11:41:21 -0400

    Strip the leading sign when printing a positive first term coefficient.

--- src/com/swtanalytics/math/Term.java
+++ src/com/swtanalytics/math/Term.java
@@ -2,10 +2,12 @@ package com.swtanalytics.math;
 
 public class Term implements Comparable<Term> {
 
+	protected static final Fraction ZERO = new Fraction(0, 1);
+
 	protected Fraction coefficient;
 	protected Fraction exponent;
-	
-	public Term (Fraction c, Fraction e){
+		
+	public Term (Fraction c, Fraction e) {
 		this.coefficient = c;
 		this.exponent = e;
 	}
@@ -14,14 +16,21 @@ public class Term implements Comparable<Term> {
             // Not smart about string construction. Just dumb manipulation
 
             String result = this.toString();
-            if (!isFirstTerm) {
+            if (isFirstTerm) {
+            	// Strip a positive sign from the first term
+            	if (ZERO.compareTo(coefficient) > 0) {
+                	result = result.substring(1, result.length());            		
+            	}
+            }
+            else {
                 result = result.substring(0,1) + ' '
                          + result.substring(1, result.length());
             }
+            
             return result;
     }
 	
-	public String toString(){
+	public String toString() {
         String exp = this.exponent.formatString(true);
 		return String.format("%sx^%s", this.coefficient, exp);
 	}
