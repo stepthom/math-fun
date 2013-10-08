3121438f36e3ad51bfc2afd48ee76d9bedeba705
Sean Mason <mason.s@gmail.com>
2013-10-07 21:27:32 -0500

    Renaming RandomIntegerGeneratorUsingMathRandom

--- src/com/swtanalytics/math/RandomGeneratorUsingMathRandom.java
+++ src/com/swtanalytics/math/RandomGeneratorUsingMathRandom.java
@@ -1,6 +1,6 @@
 package com.swtanalytics.math;
 
-public class RandomIntegerGeneratorUsingMathRandom implements RandomGenerator {
+public class RandomGeneratorUsingMathRandom implements RandomGenerator {
     @Override
     public int generateInt(int minInclusive, int maxInclusive) {
         int width = maxInclusive - minInclusive + 1;
