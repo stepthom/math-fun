9eed70ebf8b2784100d53647efbd87767e9c8bed
johns1342 <johns1342@gmail.com>
2013-12-17 16:39:06 -0500

    Changes for Issue #58 MathFunction.java  - Cleaned up equals() and hash() functions to be a little cleaner MathFunctionTest.java  - Added equalsTests() function which attempts to find issues with sort order, dead Terms (0 coefficients), other local non-equal variables, fraction and integer equivalents, and integral and derivative equality

--- src/com/swtanalytics/math/MathFunctionTest.java
+++ src/com/swtanalytics/math/MathFunctionTest.java
@@ -87,4 +87,65 @@ public class MathFunctionTest {
         // Assert
         Assert.assertEquals(new Fraction(0, 101), result);
     }
+    
+    @Test
+    public void equalsTests() {
+    	// Arrange
+    	MathFunction f1 = new MathFunction();
+    	MathFunction f2 = new MathFunction();
+    	MathFunction f3 = null;
+    	MathFunction f4 = null;
+
+    	// Act
+    	f1.addTerm(new Term(13,0));
+    	f1.addTerm(new Term(0, 4));
+       	f1.addTerm(new Term(new Fraction(4,2), new Fraction(-4,3)));
+       	f1.addTerm(new Term(3,3));
+       	f1.addTerm(new Term(1,5));
+
+       	f2.addTerm(new Term(new Fraction(10,3), 4));
+       	f2.addTerm(new Term(1,5));
+    	f2.addTerm(new Term(3,3));
+       	f2.addTerm(new Term(1, new Fraction(-4,3)));
+       	f2.addTerm(new Term(1, new Fraction(-4,3)));
+       	f2.addTerm(new Term(new Fraction(-20,6), 4));
+       	f2.addTerm(new Term(0, 13));
+       	f2.addTerm(new Term(13,0));
+
+       	boolean equals1 = f1.equals(f2);
+       	boolean equals2 = f2.equals(f1);
+       	
+       	// ensure other locals aren't being checked
+       	f1.integrate();
+       	f2.differentiate();
+       	
+       	boolean equals3 = f1.equals(f2);
+       	boolean equals4 = f2.equals(f1);
+
+       	f2.addTerm(new Term(1,3));
+       	
+       	boolean notEquals1 = f1.equals(f2);
+       	boolean notEquals2 = f2.equals(f1);
+       	
+       	boolean notEquals3 = f1.equals(f3);
+       	
+       	f3 = new MathFunction();
+       	boolean notEquals4 = f1.equals(f3);
+       	
+       	f3 = f2.integrate();
+       	f4 = f3.differentiate();
+       	boolean equals5 = f2.equals(f4);
+       	boolean equals6 = f4.equals(f2);
+       	
+       	f1.addTerm(new Term(-13,0));  // drop constant
+       	f3 = f1.differentiate();
+       	f4 = f3.integrate();
+       	boolean equals7 = f1.equals(f4);
+       	boolean equals8 = f4.equals(f1);
+       	
+    	// Assert
+    	Assert.assertTrue(equals1 && equals2 && equals3 && equals4 && 
+    			equals5 && equals6 && equals7 && equals8);
+    	Assert.assertFalse(notEquals1 || notEquals2 || notEquals3 || notEquals4);
+    }
 }
